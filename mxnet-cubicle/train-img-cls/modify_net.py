067b2923ef0e59fa73b7c7520529546350ece64d
Northrend <286424061@qq.com>
2017-12-12 13:55:34 +0800

    add modify_net.py

---
+++ mxnet-cubicle/train-img-cls/modify_net.py
@@ -0,0 +1,52 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import sys
+import mxnet as mx
+import pprint
+
+
+def modify_net(sym):
+    '''
+    write your network-modify codes here
+    '''
+    input_shape = (1, 3, 352, 352)   # input image size, (batch, channel, height, width)
+    s4_u3_c3 = sym.get_internals()['stage4_unit3_conv3_output']
+    s5_u1_c1 = mx.sym.Convolution(data=s4_u3_c3, num_filter=4096, kernel=(3, 3), stride=(
+        2, 2), pad=(1, 1), no_bias=True, workspace=512, name='stage5_unit1_conv1')
+    s5_u1_b1 = mx.sym.BatchNorm(data=s5_u1_c1, fix_gamma=False, eps=2e-5, momentum=0.9, name='stage5_unit1_bn1')
+    s5_u1_a1 = mx.sym.Activation(data=s5_u1_b1, act_type='relu', name='stage5_unit1_relu1')
+    pool = mx.symbol.Pooling(data=s5_u1_a1, global_pool=True, kernel=(6, 6), pool_type='avg', name='pool1')
+    flatten = mx.symbol.Flatten(data=pool, name='flatten0')
+    fc = mx.symbol.FullyConnected(data=flatten, num_hidden=1000, name='fc1')
+    softmax = mx.symbol.SoftmaxOutput(data=fc, name='softmax')
+
+    # ---- print newly added layers output dim ----
+    print('==> Changed layers output dimension:')
+    print('stage4_unit3_conv3:', s4_u3_c3.infer_shape(data=input_shape)[1])
+    print('stage5_unit1_conv1:', s5_u1_c1.infer_shape(data=input_shape)[1])
+    print('stage5_unit1_bn1:', s5_u1_b1.infer_shape(data=input_shape)[1])
+    print('stage5_unit1_relu1:', s5_u1_a1.infer_shape(data=input_shape)[1])
+    print('pool1:', pool.infer_shape(data=input_shape)[1])
+    print('flatten1:', flatten.infer_shape(data=input_shape)[1])
+    print('fc1:', fc.infer_shape(data=input_shape)[1])
+    print('softmax:', softmax.infer_shape(data=input_shape)[1])
+    # ---------------------------------------------
+
+    print('==> New params:')
+    pprint.pprint(zip(softmax.list_arguments(), softmax.infer_shape(data=input_shape)[0]))
+    return softmax
+
+
+def main():
+    sym, arg_params, aux_params = mx.model.load_checkpoint(sys.argv[1], 0)   # load original model
+    print('==> Original params:')
+    pprint.pprint(zip(sym.list_arguments(), sym.infer_shape(data=(1, 3, 224, 224))[0]))
+    sym_new = modify_net(sym)
+    mx.model.save_checkpoint(sys.argv[1] + '-modified', 0, sym_new, arg_params, aux_params)
+    print('New model saved at: {}'.format(sys.argv[1] + '-modified'))
+
+
+if __name__ == '__main__':
+    main()
