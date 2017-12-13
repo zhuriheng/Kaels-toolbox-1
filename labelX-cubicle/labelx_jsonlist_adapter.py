e5195b2afd86287d0367b10e107086afbe64684f
Northrend <286424061@qq.com>
2017-12-13 20:06:38 +0800

    update labelx_jsonlist_adapter.py v1.1

--- labelX-cubicle/labelx_jsonlist_adapter.py
+++ labelX-cubicle/labelx_jsonlist_adapter.py
@@ -17,16 +17,17 @@ import docopt
 def _init_():
     '''
     Script for converting labelX-standard jsonlist to groundtruth file
-    Update: 2017/12/07
+    Update: 2017/12/13
     Author: @Northrend
     Contributor: 
 
     Change log:
+    2017/12/13  v1.1          support detection
     2017/12/07  v1.0          basic functions
 
     Usage:
-    labelx_jsonlist_adapter.py          <in-list> <out-file> 
-                                        [-c|--classification -d|--detection -l|--clustering]
+    labelx_jsonlist_adapter.py          <in-list> <out-file> [-c|--classification]
+                                        [ -d|--detection -l|--clustering ]
                                         [--sub-task=str --label=str]
     labelx_jsonlist_adapter.py          -v | --version
     labelx_jsonlist_adapter.py          -h | --help
@@ -73,8 +74,8 @@ def main():
     input_file = open(args['<in-list>'], 'r')
     output_file = open(args['<out-file>'], 'w')
     json_lst = input_file.readlines()
-    assert args['--label'], 'index-classname mapping file is required.'
     if args['--classification']:
+        assert args['--label'], 'index-classname mapping file is required.'
         assert args['--sub-task'], 'sub-task should be provided in classification.'
         category = get_category(args['--label'])
         for item in json_lst:
@@ -86,6 +87,26 @@ def main():
                 print('syntax error:',item)
                 continue
             output_file.write('{} {}\n'.format(img, label))
+    elif args['--detection']:
+        dict_ann = dict()
+        for item in json_lst:
+            temp_dict = json.loads(item.strip())
+            img = os.path.basename(temp_dict['url'])
+            if img not in dict_ann:
+                dict_ann[img] = list()
+            try:
+                for instance in temp_dict['label']['detect']['general_d']['bbox']:
+                    ins_ann = list()
+                    ins_ann.append(instance['pts'][0][0])
+                    ins_ann.append(instance['pts'][0][1])
+                    ins_ann.append(instance['pts'][2][0])
+                    ins_ann.append(instance['pts'][2][1])
+                    ins_ann.append(instance['class'])
+                    dict_ann[img].append(ins_ann)
+            except:
+                print('syntax error or no object:',item.strip())
+                continue
+        json.dump(dict_ann, output_file, indent=4)
     output_file.close()
     input_file.close()
 
