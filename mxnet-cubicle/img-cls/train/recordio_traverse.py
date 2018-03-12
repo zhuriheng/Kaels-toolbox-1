39492ac31160e4f8667d7bca438a33886d640fc9
Northrend <286424061@qq.com>
2018-03-12 16:35:16 +0800

    update recordio_traverse.py

--- mxnet-cubicle/img-cls/train/recordio_traverse.py
+++ mxnet-cubicle/img-cls/train/recordio_traverse.py
@@ -1,11 +1,12 @@
 import mxnet as mx
 import sys
 
+# reccordio_traverse.py /path/to/test.rec 16
 data_iter = mx.io.ImageRecordIter(
   path_imgrec=sys.argv[1], # The target record file.
   preprocess_threads=32,
-  data_shape=(3, -1, -1), # Output data shape; 227x227 region will be cropped from the original image.
-  batch_size=128, # Number of items per batch.
+  data_shape=(3, 224, 224), # Output data shape; 227x227 region will be cropped from the original image.
+  batch_size=int(sys.argv[2]), # Number of items per batch.
   resize=256 # Resize the shorter edge to 256 before cropping.
   # You can specify more augmentation options. Use help(mx.io.ImageRecordIter) to see all the options.
   )
