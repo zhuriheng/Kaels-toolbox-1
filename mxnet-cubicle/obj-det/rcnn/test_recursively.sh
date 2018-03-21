9a05548c399ef27ceb3e06a86e8caaaf3ffa7a26
Northrend <286424061@qq.com>
2018-03-21 19:48:38 +0800

    update test_recursively.sh

--- mxnet-cubicle/obj-det/rcnn/test_recursively.sh
+++ mxnet-cubicle/obj-det/rcnn/test_recursively.sh
@@ -4,16 +4,16 @@ cd /workspace/Kaels-toolbox/mxnet-cubicle/obj-det/rcnn/
 # export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
 
 names='abdominal_muscles bangs beard black_frame_glasses black_socks boxers briefs calf feet leather_shoes pecs shorts stud_earrings suit sun_glasses tattoo under_shirt white_socks'
-for a in `seq 5 5 5`;do
+for a in `seq 2 2 14`;do
 epoch=$a
 map=0
-log_file='/workspace/blued/log-test/blued_v9.1_on_0920_test.log'
+log_file='/workspace/blued/log-test/blued_v2.1_on_0920_test.log'
 echo '==> epoch:'$epoch >> $log_file 
 echo "| Class Index | Class Name          | AP(%)   | IoU  | Max-Rec(%) |" >> $log_file
 echo "| ----------- | ------------------- | ------- | ---- | ---------- |" >> $log_file
 for i in $names;do
 ap=0;
-python -u test.py --dataset Blued --image_set 0920-$i --root_path /workspace/blued/rcnn-cache/test/ --dataset_path /workspace/dataset/blued/blued_0920/ --network resnet --prefix /workspace/blued/model/blued_v9.1_resnet/blued-v9.1 --epoch $a --thresh 0.8 | grep -v 'non' | grep $i | tail -n 1 >> $log_file;
+python -u test.py --dataset Blued --image_set 0920-$i --root_path /workspace/blued/rcnn-cache/test/ --dataset_path /workspace/dataset/blued/blued_0920/ --network resnet --prefix /workspace/blued/model/blued_v2_resnet/e2e --epoch $a --thresh 0.8 | grep -v 'non' | grep $i | tail -n 1 >> $log_file;
 ap=`tail -n 1 $log_file | cut -f4 -d'|'` ;
 map=`echo $map+$ap|bc`;
 # echo $map$ap
