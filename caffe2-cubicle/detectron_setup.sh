a288179b67ffe32644bd46b16252520d0f59bd3f
Northrend <286424061@qq.com>
2018-05-04 15:51:11 +0800

    update detectron_setup.sh

--- caffe2-cubicle/detectron_setup.sh
+++ caffe2-cubicle/detectron_setup.sh
@@ -1,14 +1,16 @@
-# test caffe2
-echo "==> Testing caffe2 env..."
-cd ~
-CAFFE2_PATH="/opt/caffe2"
-python -c 'from caffe2.python import core' 2>/dev/null && echo "Success" || echo "Failure"
-python ${CAFFE2_PATH}/caffe2/python/operator_test/relu_op_test.py
-
 # dependencies
 echo "==> Installing dependencies..."
 pip install --upgrade pip 
+pip install hypothesis
 pip install numpy>=1.13 pyyaml>=3.12 matplotlib opencv-python>=3.2 setuptools Cython mock scipy -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+# test caffe2
+echo "==> Testing caffe2 env..."
+cd ~
+ldconfig
+CAFFE2_PATH="/opt/caffe2"
+python -c 'from caffe2.python import core' 2>/dev/null && echo "Success" || echo "Failure"
+python ${CAFFE2_PATH}/caffe2/python/operator_test/relu_op_test.py
 
 # install cocoapi
 echo "==> Installing MS-COCO api..."
