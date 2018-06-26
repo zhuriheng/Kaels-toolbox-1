d2b3f56803cbdfe6d6080e73f4c203b5600ca7af
Northrend <286424061@qq.com>
2018-06-26 11:43:22 +0800

    update pytorch_setup.sh

--- pytorch-cubicle/pytorch_setup.sh
+++ pytorch-cubicle/pytorch_setup.sh
@@ -1,8 +1,9 @@
 # dependencies
-pip install upgrade
+pip install --upgrade pip
 pip install numpy pyyaml mkl mkl-include setuptools cmake cffi typing
 
 # pytorch
+cd /opt/
 git clone --recursive https://github.com/pytorch/pytorch
 cd pytorch
 python setup.py install
